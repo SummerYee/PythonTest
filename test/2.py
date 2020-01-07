fd7d32d83d79ef04af121d15379862a0eb94fa71
王小易 <1092638525@qq.com>
2020-01-07 12:45:40 +0800

    练习

---
+++ test/2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Author  : 王小易 / SummerYee
+# @Time    : 12:44 
+# @File    : Python Test 2.py
+# @Software: PyCharm
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import statsmodels as sm
