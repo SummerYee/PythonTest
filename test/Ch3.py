e42774d14c82aa1763db72b23e63d45a2982af6e
王小易 <1092638525@qq.com>
2020-01-13 18:24:53 +0800

    Initial commit

---
+++ test/Ch3.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Author  : 王小易 / SummerYee
+# @Time    : 2020/1/13 18:17
+# @File    : Python Test Ch3.py
+# @Software: PyCharm
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import statsmodels as sm
