d42e512f2e7ec91d5948976d5ee7f40e5819fab2
王小易 <1092638525@qq.com>
2020-01-13 18:15:45 +0800

    Initial commit

--- test/ipython_script_test.py
+++ test/ipython_script_test.py
@@ -16,3 +16,36 @@ a = 5
 b = 6
 c = 7.5
 result = f(a, b, c)
+
+
+# None 作为默认参数
+def add_abd_amybe_multiply(a, b, c=None):
+    result = a + b
+
+    if c is not None:
+        result = result * c
+
+    return result
+
+# 控制流 if elif else
+
+
+# for 循环
+for value in collection:
+    # 用值做些什么
+for a, b, c in iterator:
+    # 做些什么
+
+# while 循环
+
+# pass “什么都不做”
+
+# range 返回生成一个等差整数序列
+# range 产生的整数包含起始但不包含结尾  常用于根据序列的检索遍历
+
+# 三元表达式  value = true-expr if cindition else false-expr
+# 等价于    if cindition:
+#               value =  true-expr
+#           else:
+#               value = false-expr
+
