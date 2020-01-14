4ce561d208873f138aa721e857b219907c6006f8
王小易 <1092638525@qq.com>
2020-01-14 11:35:46 +0800

    Initial commit

--- test/Ch3.py
+++ test/Ch3.py
@@ -9,3 +9,17 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 import statsmodels as sm
+
+# bisect 二分搜索 已排序列表的插值
+# bisect.bisect 找到元素应当被插入的位置，并保持序列排序
+# bisect.insort 将元素插入到相应的位置
+
+
+# 内建函数 enumerate  返回(i, value) i是元素的索引 value是元素的值
+i = 0
+for value in collection:
+    # 使用值做点事
+    i += 1
+# 等价于
+for i, value in enumerate(collection):
+    # 使用值做点事
