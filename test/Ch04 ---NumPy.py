7cd6bba97fb729409ff58d148252c606ac98e989
王小易 <1092638525@qq.com>
2020-01-26 17:15:46 +0800

    Initial commit

--- test/Ch04 ---NumPy.py
+++ test/Ch04 ---NumPy.py
@@ -10,3 +10,7 @@ import pandas as pd
 import seaborn as sns
 import statsmodels as sm
 
+# NumPy 基础：数组和向量化计算
+# 在数据处理、清洗、构造子集、过滤、变换以及其他计算中进行快速的向量滑计算
+# 常见的数组算法：比sort、unique以及set操作等
+# 高效的描述性统计和聚合/概述数据
