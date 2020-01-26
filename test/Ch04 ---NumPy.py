2abb5e187d44d5b527ca12c26a487e696647d66d
王小易 <1092638525@qq.com>
2020-01-26 17:59:49 +0800

    Initial commit

--- test/Ch04 ---NumPy.py
+++ test/Ch04 ---NumPy.py
@@ -17,3 +17,31 @@ import statsmodels as sm
 # 数据排列和相关数据操作，例如对异构数据进行merge和join
 # 使用数组表达式来表明条件逻辑，代替if-elif-else条件分支的循环
 # 分组数据的操作（聚合、变换以及函数式操作）
+
+# numpy 比 python 快10 - 100 倍 并且使用内存小
+In[7]: import numpy as np
+In[8]: my_arr = np.arange(1000000)
+In[9]: my_list = list(range(1000000))
+In[10]: %time for _ in range(10): my_arr2 = my_arr * 2
+Wall time: 148 ms
+In[11]: %time for _ in range(10): my_list2 = [x * 2 for x in my_list]
+Wall time: 1.34 s
+Compiler : 121 ms
+Parser   : 256 ms
+
+# 4.1 NumPy ndarray: 多维数组对象（快速灵活的大型数据集容器）
+In[12]: import numpy as np
+In[13]: data = np.random.randn(2,3)
+In[14]: data
+Out[14]:
+array([[ 0.61080692, -0.18762617, -1.66599111],
+       [-0.14889702, -0.57375967, -0.24480561]])
+In[15]: data * 10
+Out[15]:
+array([[  6.10806917,  -1.87626174, -16.65991106],
+       [ -1.48897022,  -5.73759674,  -2.44805613]])
+In[16]: data + data
+Out[16]:
+array([[ 1.22161383, -0.37525235, -3.33198221],
+       [-0.29779404, -1.14751935, -0.48961123]])
+
